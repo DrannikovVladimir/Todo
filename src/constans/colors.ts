10eabbdff9b5f524cf6facd062d295d4d45062e9
Vladimir Drannikov <krowen@inbox.ru>
2021-10-04 15:01:45 +0600

    Added colors to constants

---
+++ src/constans/colors.ts
@@ -0,0 +1,15 @@
+const colors = {
+  primeColor: '#ffffff',
+  simpleColor: '#aaaaaa',
+  simpleColorHover: '#8a8a8a',
+  simpleColorActive: '#4b4b4b',
+  accentColor: '#59bce4',
+  accentColorHover: '#59bce4',
+  accentColorActive: '#1c95c5',
+  textColor: '#777777',
+  dangerColor: '#eb9a9a',
+  dangerColorHover: '#e44a4a',
+  dangerColorActive: '#c02424',
+}
+
+export default colors;
