bbbd1f2dbf0f8f0ccb4532147fa1698bea1d72ce
Vladimir Drannikov <krowen@inbox.ru>
2021-10-04 09:22:06 +0600

    Added app init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { observer } from 'mobx-react-lite';
 
-function App() {
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import store from './store';
+import ITodo from './interfaces';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 550px;
+  margin: 0 auto;
+  margin-top: 50px;
+  padding: 40px;
+  box-shadow: 0 0 10px 1px #e5e5e5;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  padding: 0;
+  margin-bottom: 20px;
+
+  color: #777777;
+  font-size: 42px;
+  line-height: 52px;
+`;
+
+const App:React.FC = () => {
+  const todos = store.todos;
+  const initTodos = store.initTodos;
+
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[];
+    initTodos(saved);
+  }, [initTodos]);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Title>Todo List</Title>
+      <TodoForm />
+      <TodoList />
+    </Container>
+  )
 }
 
-export default App;
+export default observer(App);
