022b6852f4aec47d556b13744f2fbbd1cc80c86a
Vladimir Drannikov <krowen@inbox.ru>
2021-10-06 15:50:03 +0600

    Added toggle translation

---
+++ src/components/LangToggle.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled from 'styled-components';
+import { observer } from 'mobx-react-lite';
+import { useTranslation } from 'react-i18next';
+
+import colors from '../constants/colors';
+
+interface IActiveLang {
+  readonly isActive: boolean;
+}
+
+const ButtonsWrapper = styled.div`
+  position: absolute;
+
+  top: 20px;
+  right: 20px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 60px;
+  height: 30px;
+  border: none;
+`;
+
+const Button = styled.button<IActiveLang>`
+  width: 30px;
+  height: 25px;
+  padding: 0;
+  border: none;
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: transparent;
+
+  font-size: 18px;
+  line-height: 28px;
+  text-align: center;
+  color: ${props => props.isActive ? colors.simpleColorActive : colors.simpleColor};
+
+  background-color: transparent;
+
+  cursor: ${props => props.isActive ? 'default' : 'pointer'};
+
+  &:hover {
+    border-bottom-color: ${props => props.isActive ? 'transparent' : colors.accentColorHover};
+
+    color: ${props => props.isActive ? colors.simpleColorActive : colors.accentColorHover};
+  }
+`;
+
+const LangToggle: React.FC = () => {
+  const { i18n } = useTranslation();
+  const handleChangeLang = (newLang: string) => (): void => {
+    i18n.changeLanguage(newLang);
+  };
+
+  return (
+    <ButtonsWrapper>
+      <Button
+        onClick={handleChangeLang('en')}
+        isActive={i18n.resolvedLanguage === 'en'}
+      >
+        En
+      </Button>
+      <Button
+        onClick={handleChangeLang('ru')}
+        isActive={i18n.resolvedLanguage === 'ru'}
+      >
+        Ru
+      </Button>
+    </ButtonsWrapper>
+  );
+};
+
+export default observer(LangToggle);
